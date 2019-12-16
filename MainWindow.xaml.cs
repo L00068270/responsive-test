fe168181e0428e91244bbf82cea89ee1ab703436
L00068270 <L00068270@student.lyit.ie>
2019-12-16 09:22:08 +0000

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace responsive_test
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btIndex(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnRecords(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnRegister(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnLogin(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+      
+    }
+}
